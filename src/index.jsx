10b01126aa7a3d169cbaf5f2e28fe775942d51b9
carineortolani <carine.ortolani@liveonsolutions.com>
2021-09-16 17:29:34 -0300

    feat(calculator): adding calculator

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Calculator from './Calculator'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
