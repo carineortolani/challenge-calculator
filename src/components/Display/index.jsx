df206d155048c6644968c9ea79772608907ae601
carineortolani <carine.ortolani@liveonsolutions.com>
2021-09-17 13:01:32 -0300

    feat(display): adding display

---
+++ src/components/Display/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Displayed } from './styles'
+
+const Display = ({ displayValue }) => (
+  <Displayed>
+    {displayValue}
+  </Displayed>
+)
+
+export default Display
---
+++ src/components/Display/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Displayed = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  grid-column: span 4;
+  padding: 20px;
+  margin-top: 20px;
+
+  font-size: 2em;
+  color: #fff;
+
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  background-color: #0004;
+  overflow: hidden;
+`
